f1d478d5183c20fa8bf25528eab143e02008addc
unknown <usuario1@M2-305-26-11385.utadeo.loc>
2024-02-08 19:29:40 -0500

    start program

---
+++ Untitled-1.dart
@@ -0,0 +1,20 @@
+void main (List<String> arguments){
+  String variableTexto ="Hola mundo";
+int numerosEnteros = 15;
+double numeroDouble =3.7;
+boo esVerdad= true;
+
+var esUnTexto "jsajkdsdajdadkadj";
+
+
+dynamic esEsNumero =14;
+esNumero= 14;
+
+
+
+print(variableTexto);
+
+String name;
+
+
+}
