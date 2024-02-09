3a9cd582045d443684425ec82e50d1b24b464cef
unknown <usuario1@M2-305-26-11385.utadeo.loc>
2024-02-08 19:45:44 -0500

    actualizacion

--- Untitled-1.dart
+++ Untitled-1.dart
@@ -17,4 +17,48 @@ print(variableTexto);
 String name;
 
 
+var arreglo =[1,2,3,4,5,6,7,8,9];
+
+List<int> lista =[1,2,3,4,5,6,7,8,9];
+
+List<String> listaLetras =["","","",""];
+
+//comentario de una sola linea
+
+/*
+comentarios de varias lineas
+*/
+
+if(numerosEnteros ==15){
+  print("la variable es igual a 15");
+}else if(numerosEnteros ==1){
+  print("la variable es igual a 1");
+}else{
+  print("no sabemos el valor de la variable");
+}
+
+switch(numerosEnteros){
+  case 15:
+    print("la variable es igual a 15");
+  break;
+}
+
+
+for(int 1= 0; i< numerosEnteros; i++){
+
+}
+
+}
+
+int sumaDosNumeros(int numberOne, int numberTwo){
+  return numberOne + numberTwo;
+
+}
+String HolaMundo(){
+  return "hola mundo";
+
+}
+
+void funcion(){
+  
 }
